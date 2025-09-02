c1f2b9f1cd6c1d70c3607d11585837f27c49b2fb
Ahmed Lemssiah <art3que@gmail.com>
2025-09-02 05:04:09 +0200

    Reset All

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,13 @@
-'use client';
 
-import { useState, useEffect } from 'react';
 import Container from "@/components/shared/Container";
 import Section from "@/components/shared/Section";
-import PageSkeleton from "@/components/shared/PageSkeleton";
-import TodoDashboard from "@/components/Todo/TodoDashboard";
-import { sampleTodos, calculateTodoStats } from "@/data/sampleTodos";
-import { Todo } from "@/types/todo";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    // Simuliere Ladezeit für Daten
-    const timer = setTimeout(() => {
-      setTodos(sampleTodos);
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const handleTodoToggle = (id: string) => {
-    setTodos(prevTodos =>
-      prevTodos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const handleTodoEdit = (id: string) => {
-    console.log('Edit todo:', id);
-    // Hier würde die Bearbeitungslogik implementiert
-  };
-
-  const handleTodoDelete = (id: string) => {
-    console.log('Delete todo:', id);
-    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
-  };
-
-  if (isLoading) {
-    return <PageSkeleton />;
-  }
-
-  const stats = calculateTodoStats(todos);
-
   return (
     <>
       <Section>
         <Container>
-          <TodoDashboard 
-            todos={todos}
-            stats={stats}
-            onTodoToggle={handleTodoToggle}
-            onTodoEdit={handleTodoEdit}
-            onTodoDelete={handleTodoDelete}
-          />
+          Home
         </Container>
       </Section>
     </>
